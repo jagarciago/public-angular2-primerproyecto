26c1d708d852433c5e709c869bd156aa0e8bc04c
jagarciago <alex.garciag@outlook.com>
2016-10-29 14:19:34 -0500

    crear index

---
+++ src/domain/artista.model.ts
@@ -0,0 +1,11 @@
+ class Artista
+{
+    public nombre: string;
+    public edad: number;
+
+    constructor(){ }
+    public presentarse()
+    {
+        alert ("hola soy:" + this.nombre);
+    }
+}
