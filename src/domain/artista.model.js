8a663517ebbe89066ca172a5286f08778cf3b44b
jagarciago <alex.garciag@outlook.com>
2016-10-29 14:20:15 -0500

    comp

--- src/domain/artista.model.js
+++ src/domain/artista.model.js
@@ -1,8 +1,9 @@
-"use strict";
 var Artista = (function () {
     function Artista() {
     }
+    Artista.prototype.presentarse = function () {
+        alert("hola soy:" + this.nombre);
+    };
     return Artista;
 }());
-exports.Artista = Artista;
 //# sourceMappingURL=artista.model.js.map
